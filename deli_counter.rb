e0c03d6d10ec0c1790efcde13e2781ecbb1af335
Maverick Adolfo <merkonical@gmail.com>
2019-08-19 03:21:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,6 @@
 # Write your code here.
 
 def line(katz_deli)
-  katz_deli = []
-  puts "The line is currently empty."
-end 
-
-def line(other_deli)
-  if other_deli == 0 
-    puts "The line is currently empty."
-  else other_deli 
+  if katz_deli.empty?
+    puts "the line is currently empty."
 end 
